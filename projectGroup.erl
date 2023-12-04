809eebf9efbac145b86cfb2218daca8a924664eb
unknown <zeliadecaillet1@gmail.com>
2023-12-04 14:50:54 +0100

    New project file

---
+++ projectGroup.erl
@@ -0,0 +1,34 @@
+-module(projectGroup).
+-export([start/1, add/3, notify/2]).
+
+%List of members%
+loop(Id, Members) ->
+    receive
+        {From, {add, Member}} ->
+            From ! {self(), ok},
+            notify(Members, Member),
+            loop(Id, [{Member, Id}| Members]);
+        {From, get_members} ->
+            From ! {self(), Members},
+            loop(Id, Members)
+    end.
+
+%add a member%
+add(Server, Id, Member) ->
+    Server ! {self(), {add, Member, Id}},
+    receive
+        {Server, ok} ->
+            ok
+    end.
+
+%notify all members of the group about the new member%
+notify (Members, NewMember) ->
+    lists:foreach(fun({Member, Id})->
+        Member ! {new_member, NewMember},
+        receive 
+            {Id, acknowledged} -> ok
+        end
+    end, Members).
+
+start(Id) ->
+    spawn(fun()-> loop(Id, []) end).
