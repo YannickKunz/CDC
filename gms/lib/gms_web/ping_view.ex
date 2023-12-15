3324a6183fbdf8951b789f4773b49dbfdefab29b
YannickKunz <yannick.kuenzli@gmail.com>
2023-12-15 11:49:01 +0100

    ADded JS and CSS to ping_view.ex

--- gms/lib/gms_web/ping_view.ex
+++ gms/lib/gms_web/ping_view.ex
@@ -14,6 +14,109 @@ defmodule GmsWeb.PingView do
     <div class="grid grid-cols-3 gap-4">
       <p>Response: <%= @response %></p>
     </div>
+    <div class="card">
+            <div class="card-body">
+                <h1 class="card-title">Distributed Membership Service with Multicast</h1>
+            </div>
+        </div>
+        <div class="card mt-3">
+            <div class="card-body">
+                <input type="text" id="createGroupName" placeholder="Group Name">
+                <input type="number" id="numberOfProcesses" placeholder="Number of Processes">
+                <.button class="btn btn-primary" id="createGroup" onclick="createGroup()">Create group</.button>
+            </div>
+        </div>
+        <div id="groupsContainer" class="groups-container"></div>
+        <script>
+        function createGroup() {
+            var groupName = document.getElementById('createGroupName').value;
+            var numberOfProcesses = document.getElementById('numberOfProcesses').value;
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", '/start', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify({
+              "group_name": groupName,
+              "number_of_processes": numberOfProcesses
+            }));
+
+            xhr.onload = function() {
+              if (xhr.status == 200) {
+                // Create a new div for the group
+                var groupDiv = document.createElement('div');
+                groupDiv.className = 'group';
+                groupDiv.innerHTML = '<h2>' + groupName + '</h2>';
+
+                // Add process names to the div
+                for (var i = 0; i < numberOfProcesses; i++) {
+                  var processName = 'Process ' + (i + 1);
+                  var processP = document.createElement('p');
+                  processP.textContent = processName;
+                  groupDiv.appendChild(processP);
+                }
+
+                // Add the div to the groups container
+                var groupsContainer = document.getElementById('groupsContainer');
+                groupsContainer.appendChild(groupDiv);
+              }
+            };
+          }
+        </script>
+        <style>
+        .card {
+          box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+          transition: 0.3s;
+          width: 80%;
+          border-radius: 5px;
+          margin: auto;
+          padding: 20px;
+      }
+
+      .card:hover {
+          box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+      }
+
+      .container {
+          padding: 2rem;
+          text-align: center;
+      }
+
+      .btn-primary {
+          background-color: #007bff;
+          border-color: #007bff;
+          color: #fff;
+          margin-top: 5px;
+          margin-left: auto;
+          text-align: center;
+      }
+
+      .groups-container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: space-between;
+      }
+
+      .card {
+          flex: 0 0 calc(50% - 20px);
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+      }
+
+      .card-body {
+          display: block;
+          flex-direction: column;
+          align-items: center;
+      }
+
+      .btn {
+          padding: 5px 10px;
+          width: 150px;
+          display: block;
+          margin: 5px auto;
+      }
+
+        </style>
     """
   end
 
