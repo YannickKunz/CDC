c342586e503b8bb89936702895a775f802f02212
Fabiennevn <fabienne.vonniederhaeusern@unifr.ch>
2023-12-22 20:23:48 +0100

    deleted unnecessary lines

--- gms/lib/gms/application.ex
+++ gms/lib/gms/application.ex
@@ -8,17 +8,11 @@ defmodule Gms.Application do
   def start(_type, _args) do
     # Create a node for the elixir frontend
     Node.start(:'elixirSide@127.0.0.1')
-    #Logger.info("self(): #{inspect(self())}")
-    #Logger.info("Local node alive: #{inspect(Node.alive?)}")
     # Connect this node with an erlang node (the erlang node needs to be created before running this code)
     Node.connect(:"erlangSide@127.0.0.1")
-    #Logger.info("Inspect Node.connect(:'erlangSide@127.0.0.1'): #{inspect(Node.connect(:"erlangSide@127.0.0.1"))}")
-    #Logger.info("Node.list(): #{inspect(Node.list())} - This shows all visible nodes in the system excluding the local node.")
     # Register a name to the local node
     Process.register(self(), :node)
-    # Send a message to the erlang node to check the connection
-    # Process.send({:node, :"erlangSide@127.0.0.1"}, {:hello, :from, self()}, [])
-    # create a group named group3 with 5 processes
+
     case :myP.start(:group3, 5) do
       {:ok, group_name} -> Logger.info("####################################################### application.ex group_name #{inspect(group_name)}")
     end
